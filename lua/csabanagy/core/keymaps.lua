0759f2f900d8602d28aa1a0acda344b3a27ed2c0
csaba-nagy <dev.csaba.nagy@gmail.com>
2025-01-15 08:00:55 +0100

    feat: update telescope config

--- lua/csabanagy/core/keymaps.lua
+++ lua/csabanagy/core/keymaps.lua
@@ -1,59 +1,55 @@
 -- set leader key to space
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 local keymap = vim.keymap -- for conciseness
 
 -- exit Insert Mode
-keymap.set('i', 'jk', '<ESC>')
-keymap.set('i', 'jK', '<ESC>')
+keymap.set("i", "jk", "<ESC>")
+keymap.set("i", "jK", "<ESC>")
 keymap.set("i", "Jk", "<ESC>")
 keymap.set("i", "JK", "<ESC>")
 
 -- exit Visual Mode
-keymap.set('v', 'jk', '<ESC>')
-keymap.set('v', 'jK', '<ESC>')
-keymap.set('v', 'Jk', '<ESC>')
-keymap.set('v', 'JK', '<ESC>')
+keymap.set("v", "jk", "<ESC>")
+keymap.set("v", "jK", "<ESC>")
+keymap.set("v", "Jk", "<ESC>")
+keymap.set("v", "JK", "<ESC>")
 
 -- clear the search highlights
-keymap.set('n', '<leader>nh', ':nohl<CR>')
+keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- delete the character under the cursor without copying it into the default register
-keymap.set('n', 'x', '"_x')
+keymap.set("n", "x", '"_x')
 
 -- increment/decrement numbers under the cursor
-keymap.set('n', '<leader>+', '<C-a>') -- increment
-keymap.set('n', '<leader>-', '<C-x>') -- decrement
+keymap.set("n", "<leader>+", "<C-a>") -- increment
+keymap.set("n", "<leader>-", "<C-x>") -- decrement
 
 -- Window management
 
-keymap.set('n', '<leader>sv', '<C-w>v')        -- split window vertically
-keymap.set('n', '<leader>sh', '<C-w>s')        -- split window horizontally
-keymap.set('n', '<leader>se', '<C-w>=')        -- make split windows equal width & height
-keymap.set('n', '<leader>sx', ':close<CR>')    -- close current split window
-keymap.set('n', '<leader>to', ':tabnew<CR>')   -- open new tab
-keymap.set('n', '<leader>tx', ':tabclose<CR>') -- close current tab
-keymap.set('n', '<leader>tn', ':tabn<CR>')     --  go to next tab
-keymap.set('n', '<leader>tp', ':tabp<CR>')     --  go to previous tab
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 --move lines in visual mode
-vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
-vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 --disable arrow buttons
-vim.keymap.set('', '<up>', '<nop>')
-vim.keymap.set('', '<down>', '<nop>')
-vim.keymap.set('', '<left>', '<nop>')
-vim.keymap.set('', '<right>', '<nop>')
+vim.keymap.set("", "<up>", "<nop>")
+vim.keymap.set("", "<down>", "<nop>")
+vim.keymap.set("", "<left>", "<nop>")
+vim.keymap.set("", "<right>", "<nop>")
 
 --magical replace command
-vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
--- Search Text
-vim.keymap.set('n', '<leader>f', [[/\<word\>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Go to definition and declaration remap (not elegant, but it works)
-keymap.set( 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-keymap.set( 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-
+keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
--- lua/csabanagy/plugins/telescope.lua
+++ lua/csabanagy/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
     local actions = require("telescope.actions")
 
     telescope.setup({
-      defaults = {
+      defaults = require("telescope.themes").get_ivy({
         prompt_prefix = " ",
         selection_caret = " ",
         path_display = { "smart" },
@@ -22,7 +22,7 @@ return {
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
           },
         },
-      },
+      }),
     })
 
     telescope.load_extension("fzf")
@@ -30,10 +30,13 @@ return {
 
     -- set keymaps
     local keymap = vim.keymap
+    local builtin = require("telescope.builtin")
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+    keymap.set("n", "<leader>f", builtin.current_buffer_fuzzy_find, { desc = "Find string in current buffer" })
   end,
 }
